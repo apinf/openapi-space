d23183303fd4f38561c9fca81f1c02317cfa2d24
Tulir Asokan <tulir@maunium.net>
2017-07-14 10:28:25 +0300

    Change commit

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-from flask import Flask
+import connexion
 from space.models import db
 
-space = Flask("OpenAPI space")
+space = connexion.App("OpenAPI space", specification_dir='./')
 space.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////space.db"
 space.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 space.config["SECRET_KEY"] = "Highly secret secret key"
+space.add_api('swagger.yaml')
 db.init_app(space)
 print("Initialization complete")
 
