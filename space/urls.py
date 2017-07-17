20517c2ec2a4a3fa4dd3b1ef7980f6924c98898b
xylix <kerk.pelt@gmail.com>
2017-07-17 10:56:04 +0300

    add lepo

---
+++ space/urls.py
@@ -0,0 +1,18 @@
+from pkg_resources import resource_filename
+
+from django.conf.urls import url, include
+from django.contrib import admin
+
+from lepo.router import Router
+from lepo.validate import validate_router
+
+from space.server import views
+
+router = Router.from_file(resource_filename(__name__, '../swagger.yaml'))
+router.add_handlers(views)
+validate_router(router)
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(router.get_urls(), 'api')),
+]
