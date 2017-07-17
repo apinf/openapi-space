46ef758accd664ac9d4f1f3716cff08c6a48fc6a
Tulir Asokan <tulir@maunium.net>
2017-07-17 15:28:20 +0300

    Improve authentication system

--- space/models/user.py
+++ space/models/user.py
@@ -1,9 +1,9 @@
 import bcrypt
-from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer,
-                          BadSignature, SignatureExpired)
-from . import db
+import hashlib
+from Crypto.Random import random
+from datetime import datetime
 
-SECRET_KEY = "replaceme"
+from . import db
 
 
 class User(db.Model):
@@ -18,19 +18,47 @@ class User(db.Model):
         return self.hashed_password == bcrypt.hashpw(password,
                                                      self.hashed_password)
 
-    def generate_auth_token(self, expiration=600):
-        s = Serializer(SECRET_KEY, expires_in=expiration)
-        return s.dumps({'username': self.name})
+    def check_auth_token(self, token):
+        tokenObj = AuthToken.get_token(self.name, token)
+        if tokenObj.expired():
+            return False
+        tokenObj.refresh()
+        db.session.update(tokenObj)
+        return tokenObj
 
-    @staticmethod
-    def read_auth_token(token):
-        s = Serializer(SECRET_KEY)
-        try:
-            data = s.loads(token)
-        except SignatureExpired:
-            # Token expired
-            return None
-        except BadSignature:
-            # Invalid token
-            return None
-        return User.query.get(name=data['username']).first()
+    # Source: https://stackoverflow.com/a/33191626
+    def generate_auth_token(self,
+                            length=64,
+                            expiry=datetime.timedelta(days=14)):
+        alnum = ''.join(c for c in map(chr, range(256)) if c.isalnum())
+        token = ''.join(random.choice(alnum) for _ in range(length))
+        hasher = hashlib.sha256()
+        hasher.update(token)
+
+        tokenObj = AuthToken(self.name, hasher.digest())
+        tokenObj.refresh(expiry)
+        db.session.add(tokenObj)
+
+
+class AuthToken(db.Model):
+    username = db.Column(
+        db.String, db.ForeignKey("user.name"), primary_key=True)
+    hashed_token = db.Column(db.String(64), primary_key=True)
+    best_before = db.Column(db.DateTime)
+
+    @classmethod
+    def get_token(cls, username, token):
+        hasher = hashlib.sha256()
+        hasher.update(token)
+        return cls.query.get(
+            username=username, hashed_token=hasher.digest()).first()
+
+    def has_expired(self):
+        if datetime.now() > self.best_before:
+            db.session.delete(self)
+            db.session.commit()
+            return True
+        return False
+
+    def refresh(self, new_time=datetime.timedelta(days=14)):
+        self.best_before = datetime.now() + new_time
