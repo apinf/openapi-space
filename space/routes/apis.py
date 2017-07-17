9af34ddd4abddadd8991d10a6f9cc484982a6d2e
Tulir Asokan <tulir@maunium.net>
2017-07-17 20:28:54 +0300

    Remove incorrect parameters

--- space/routes/apis.py
+++ space/routes/apis.py
@@ -1,34 +1,34 @@
-def delete_api(request, owner, api):
+def delete_api(owner, api):
     raise NotImplementedError('Handler delete_api not implemented')
 
 
-def delete_api_version(request, owner, api, version):
+def delete_api_version(owner, api, version):
     raise NotImplementedError('Handler delete_api_version not implemented')
 
 
-def get_api_versions(request, owner, api):
+def get_api_versions(owner, api):
     raise NotImplementedError('Handler get_api_versions not implemented')
 
 
-def get_json_definition(request, owner, api, version):
+def get_json_definition(owner, api, version):
     raise NotImplementedError('Handler get_json_definition not implemented')
 
 
-def get_owner_apis(request, owner, sort, order):
+def get_owner_apis(owner, sort, order):
     raise NotImplementedError('Handler get_owner_apis not implemented')
 
 
-def get_yaml_definition(request, owner, api, version):
+def get_yaml_definition(owner, api, version):
     raise NotImplementedError('Handler get_yaml_definition not implemented')
 
 
-def publish_api_version(request, owner, api, version):
+def publish_api_version(owner, api, version):
     raise NotImplementedError('Handler publish_api_version not implemented')
 
 
-def save_definition(request, owner, api, isPrivate, definition, force):
+def save_definition(owner, api, isPrivate, definition, force):
     raise NotImplementedError('Handler save_definition not implemented')
 
 
-def search_apis(request, query, limit, offset, sort, order):
+def search_apis(query, limit, offset, sort, order):
     raise NotImplementedError('Handler search_apis not implemented')
