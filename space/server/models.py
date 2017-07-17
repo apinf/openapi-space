84e321e64fa586d02fd57b6069389b8c28c9e044
xylix <kerk.pelt@gmail.com>
2017-07-17 12:19:45 +0300

    Add model for API files

--- space/server/models.py
+++ space/server/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Api(models.Model):
+    owner = models.ForeignKey("auth.User")
+    name = models.TextField()
+    version = models.TextField()
+    created = models.DateTimeField(
+        default=timezone.now)
+    modified = models.DateTimeField(
+        default=timezone.now)
+    swagger = models.TextField()
+
+    def __str__(self):
+return self.name
