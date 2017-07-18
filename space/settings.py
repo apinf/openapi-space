07d3b804c518b12c20c85224baf788c52ee7dd8b
xylix <kerk.pelt@gmail.com>
2017-07-18 10:33:12 +0300

    Add auth token handler to /api-token-auth

--- space/settings.py
+++ space/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     'space.lepo',
     'space.server',
 ]
@@ -119,3 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    )
+}
--- space/urls.py
+++ space/urls.py
@@ -1,5 +1,7 @@
 from pkg_resources import resource_filename
 
+from rest_framework.authtoken import views as authviews
+
 from django.conf.urls import url, include
 from django.contrib import admin
 
@@ -16,4 +18,5 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^auth/', include('django.contrib.auth.urls')),
     url(r'^api/v1/', include(router.get_urls(), 'api')),
+    url(r'^api-token-auth/', authviews.obtain_auth_token),
 ]
