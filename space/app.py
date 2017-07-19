6278420b6ee2136bad23cf3926f1a30b8c6c7a81
xylix <kerk.pelt@gmail.com>
2017-07-19 09:05:14 +0300

    Add configuration file

--- space/app.py
+++ space/app.py
@@ -1,9 +1,11 @@
 import connexion
-
-connexionSpace = connexion.App("OpenAPI space", specification_dir='./')
+import yaml
+with open("config.yaml", "r") as stream:
+    config = yaml.load(stream)
+connexionSpace = connexion.App("OpenAPI space", specification_dir="./")
 space = connexionSpace.app
-space.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///space.db"
+space.config["SQLALCHEMY_DATABASE_URI"] = config["database_uri"]
 space.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-space.config["BASE_URL"] = "https://openapi.space/api/v1"
-space.config["SECRET_KEY"] = "Highly secret secret key"
-connexionSpace.add_api('swagger.yaml')
+space.config["BASE_URL"] = config["base_url"]
+space.config["SECRET_KEY"] = config["secret_key"]
+connexionSpace.add_api("swagger.yaml")
