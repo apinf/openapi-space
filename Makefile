ee82fc8b8c9556909487b4695d988fbd858f882c
Mauricio Vieira <mauricio@mauriciovieira.net>
2017-07-24 20:36:10 -0300

    Add make lint and make test
    - Also, fix test.

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 install-ci:
 	pip install -r requirements/ci.txt
+
+lint:
+	pytest --flake8
+
+test: lint
+	py.test
--- tests/test_spec.py
+++ tests/test_spec.py
@@ -1,5 +1,9 @@
 import yaml
 from swagger_spec_validator.validator20 import validate_spec
-with open("swagger.yaml", "r") as stream:
-    specification = yaml.load(stream)
-validate_spec(specification)
+
+
+def test_validate_spec():
+    with open("swagger.yaml", "r") as stream:
+        specification = yaml.load(stream)
+    swagger_resolver = validate_spec(specification)
+    assert swagger_resolver.referrer['info']['title'] == 'OpenAPI space'
